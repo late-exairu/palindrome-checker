dbaf847fe3dc058f7b452579a815ef9f37cdbe63
Oleg Shelest <lateexairu@gmail.com>
2023-12-30 18:46:56 +0200

    Update UI

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ const check = document.querySelector("#check-btn") as HTMLButtonElement;
 const input = document.querySelector("#text-input") as HTMLInputElement;
 const result = document.querySelector("#result") as HTMLParagraphElement;
 
-const handleCheck = () => {
+const handleCheck = (e: Event) => {
+  e.preventDefault();
   const checkString = input.value;
   const regex = /[0-9a-zA-Z]/g;
   const found = checkString.match(regex)?.join("");
@@ -17,9 +18,9 @@ const handleCheck = () => {
   const foundReverse = found.split("").reverse().join("");
 
   if (found.toLowerCase() === foundReverse.toLowerCase()) {
-    result.innerHTML = `<span class="font-bold">${checkString}</span> is a palindrome`;
+    result.innerHTML = `<span class="font-bold text-green-400">${checkString}</span> is a palindrome`;
   } else {
-    result.innerHTML = `<span class="font-bold">${checkString}</span> is not a palindrome`;
+    result.innerHTML = `<span class="font-bold text-red-400">${checkString}</span> is not a palindrome`;
   }
 };
 
